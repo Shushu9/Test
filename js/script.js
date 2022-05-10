34da1462e7eaa7cdbf5c498f13c32cc0e3c23b42
Gleb <pokotilo.gleb1@gmail.com>
2022-05-10 10:44:17 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+// const result = confirm("Are you fapping?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer + 66);
+
+// console.log(4 + +"15");
+
+const isChecked = true,
+isClose = false;
+
+console.log(isChecked || isClose);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// document.write(answers);
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ", длина: " + vehicleBodyLength);
+
+// let userName = 'John'; 
+// let userNumber = 25;
+// userNumber = 24;
+
+// let storeName = 'Apple';
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Alan', 'Dane', 'Peter'],
+//     products: {
+//         apples: 665,
+//         bananas: 798
+//     },
+//     open: true
+
+// };
+
+// alert('hello');
